4f40b48087c86ca98f72a283ec09341994a49625
gabriel <gabrieldosanjosdbz@gmil.com>
2024-05-17 10:44:42 -0300

    firts commit

---
+++ src/dal/Mod_conexao.java
@@ -0,0 +1,24 @@
+package dal;
+import java.sql.*;
+
+public class Mod_conexao {
+    public static Connection  conector(){
+        java.sql.Connection conexao = null;
+        //a linha abaixo chama o driver
+        String driver = "com.mysql.cj.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/usuarios";
+        
+        String user ="root";
+        String password = "";
+        
+        try{
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url,user,password);
+            return conexao;
+            
+        }catch (Exception e){
+            return null;
+        }
+        
+    }
+}
